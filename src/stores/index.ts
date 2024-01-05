f8c399ea0de1c472447e19976fdc9c819b2494c9
Perzch <perzch@qq.com>
2024-01-05 23:58:24 +0800

    增加本地iconfont 修改基本css文件

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,66 +1,32 @@
 import {defineStore} from "pinia";
-import {ref} from "vue";
-import dayjs from "dayjs";
-import {useRouter} from "vue-router";
-type SearchInfo = {
-    homeType?: string,
-    minPrice: number,
-    maxPrice: number,
-    device?: string[],
-    address: string,
-    beginDate: string,
-    endDate: string,
-    roomCount: 1,
-    personCount: 1,
-}
-export const useGlobalizationStore = defineStore('globalization',() => {
-    const {push} = useRouter()
-    const searchInfo = ref(<SearchInfo>{})
-    const disabledCheckoutDate = (time:string) => {
-        //当填了入住日期时，禁用入住日期之前的日期
-        if(searchInfo.value?.beginDate) return dayjs(time).isBefore(dayjs(searchInfo.value?.beginDate))
-        else return dayjs(time).isBefore(dayjs(),"D")
-    }
-    const disabledCheckinDate = (time:string) => {
-        if(searchInfo.value?.endDate) return dayjs(time).isAfter(dayjs(searchInfo.value?.endDate))
-        else return dayjs(time).isBefore(dayjs(),"D")
-    }
-    const validateCheckinDate = () => {
-        if(searchInfo.value?.beginDate && searchInfo.value?.endDate) {
-            if(dayjs(searchInfo.value?.endDate).isBefore(dayjs(searchInfo.value?.beginDate))) {
-                // @ts-ignore
-                ElMessage.warning("入住日期必须大于等于当前日期");
-                searchInfo.value.beginDate = ''
-            }
-        }
-    }
+import {useStorage} from "@vueuse/core";
+import {getUserInfo as getUInfo} from "@/api/user/info";
+import type {UserInfo} from "@/types";
+import {computed} from "vue";
 
-    const search = async () => {
-        if(searchInfo.value.beginDate && searchInfo.value.endDate) {
-            await push({
-                path: '/search',
-                query: searchInfo.value
-            })
-        } else {
-            // @ts-ignore
-            ElMessage.warning('请选择入住日期和退房日期');
-        }
-    }
-    const validateCheckoutDate = () => {
-        if(searchInfo.value?.beginDate && searchInfo.value?.endDate) {
-            if(dayjs(searchInfo.value?.endDate).isBefore(dayjs(searchInfo.value?.beginDate))) {
-                // @ts-ignore
-                ElMessage.warning('退房日期必须大于等于当前日期');
-                searchInfo.value.endDate = ''
-            }
+
+/**
+ * @function useGlobalStore
+ * @description This function defines a store for globalization.
+ * @returns {Object} The store object.
+ */
+export const useGlobalStore = defineStore('global',() => {
+    const userId = useStorage('userId', '')
+    const token = useStorage('token', '')
+    const userInfo = useStorage<UserInfo>('userInfo', {} as UserInfo)
+    const isLogin = computed(() => userId.value && token.value)
+    const getUserInfo = async ():Promise<UserInfo | undefined> => {
+        if(userId.value && token.value) {
+            const {data} = await getUInfo(Number(userId.value))
+            userInfo.value = data
+            return data
         }
     }
     return {
-        searchInfo,
-        search,
-        validateCheckinDate,
-        validateCheckoutDate,
-        disabledCheckoutDate,
-        disabledCheckinDate
+        userInfo,
+        isLogin,
+        getUserInfo,
+        userId,
+        token,
     }
 })
